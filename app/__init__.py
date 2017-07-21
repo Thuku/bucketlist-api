8931cfe3883b86378f28450c6a07a0da9f4757bb
Thuku <thuku.bonnie@gmail.com>
2017-07-21 09:46:59 +0300

    [CHORE] Remove unused settings file

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+"""App initialization file."""
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
--- config.py
+++ config.py
@@ -1,2 +1,4 @@
+"""Configration file."""
+
 SQLALCHEMY_DATABASE_URI = 'sqlite:///bucketlist.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
