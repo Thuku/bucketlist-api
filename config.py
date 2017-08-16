3cf55023bf77ac0a6599cea9503465657c608c76
Thuku <thuku.bonnie@gmail.com>
2017-08-16 12:03:45 +0300

    [CHORE] Change testing db from postgres to sqlite

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 """Configration file."""
-
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     """configuration."""
@@ -11,7 +12,8 @@ class Config(object):
 class TestingConfig(Config):
     """Testing configuration."""
 
-    SQLALCHEMY_DATABASE_URI = 'postgres://test_bucketlist'
+    SQLALCHEMY_DATABASE_URI = ("sqlite:///" +
+                                os.path.join(basedir, 'test.db'))
     DEBUG = True
 
 
