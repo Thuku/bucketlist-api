74e5be6deabd607bde60934fc0ec3b8fea312c4f
Thuku <thuku.bonnie@gmail.com>
2017-08-17 04:20:51 +0300

    [CHORE] Remove secret key

--- config.py
+++ config.py
@@ -2,18 +2,19 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config(object):
     """configuration."""
 
     SQLALCHEMY_DATABASE_URI = 'postgres://bucketlist'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    ERROR_404_HELP =False
+    ERROR_404_HELP = False
+
 
 class TestingConfig(Config):
     """Testing configuration."""
 
     SQLALCHEMY_DATABASE_URI = ("sqlite:///" + os.path.join(basedir, 'test.db'))
-    SECRET_KEY="ndvbfsdjmnbfjkmnbdcbnmxvjhnmbsxnmbasdjnmmdsnmxbjhdshjcknmb"
     DEBUG = True
 
 
