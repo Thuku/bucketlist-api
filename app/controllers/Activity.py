1c4a5264f26d2a6ade467ce9a8949e14893e7105
Thuku <thuku.bonnie@gmail.com>
2017-08-17 03:58:02 +0300

    [chore] Refactor code

--- app/controllers/Activity.py
+++ app/controllers/Activity.py
@@ -1,15 +1,18 @@
 """Authentication."""
 from flask import Flask, request, make_response, json, jsonify
 from flask_restful import reqparse, request, Resource
-from app import  db
+from app import db
 from app.models.Models import User, Bucket, Activity
 from app.models.Models import logged_in
+
+
 class ActivitiesResource(Resource):
 
     @logged_in
     def get(self, bucketlist_id, user_id=None, res=None):
         if user_id is not None:
-            bucketlist = Bucket.query.filter_by(user_id=user_id, id=bucketlist_id).first()
+            bucketlist = Bucket.query.filter_by(
+                user_id=user_id, id=bucketlist_id).first()
             if bucketlist:
                 items = bucketlist.activities
                 responseObject = []
@@ -23,30 +26,31 @@ class ActivitiesResource(Resource):
                     responseObject.append(item)
                 return make_response((responseObject))
             else:
-                responseObject ={
+                responseObject = {
                     'status': 'fail',
                     'message': 'bucketlist  does not exist'
                 }
                 return make_response((responseObject))
 
     @logged_in
-    def post(self,bucketlist_id, user_id=None, res=None):
+    def post(self, bucketlist_id, user_id=None, res=None):
         parser = reqparse.RequestParser()
         parser.add_argument('name', type=str, required=True)
         args = parser.parse_args()
         if len(args['name']) < 5:
-            responseObject ={
+            responseObject = {
                 'status': 'fail',
                 'message': "Activity should be more than 5 characters"
             }
             return make_response(jsonify(responseObject))
 
-
         else:
             if user_id is not None:
-                bucketlist = Bucket.query.filter_by(id=bucketlist_id,user_id=user_id).first()
+                bucketlist = Bucket.query.filter_by(
+                    id=bucketlist_id, user_id=user_id).first()
                 if bucketlist:
-                    activity = Activity.query.filter_by(bucket_id=bucketlist_id, name=args.get('name')).first()
+                    activity = Activity.query.filter_by(
+                        bucket_id=bucketlist_id, name=args.get('name')).first()
                     if not activity:
                         activity = Activity(
                             name=args.get('name'),
@@ -56,13 +60,13 @@ class ActivitiesResource(Resource):
                         db.session.commit()
                         responseObject = {
                             'status': 'successful',
-                            'message':'Activity successfuly created'
+                            'message': 'Activity successfuly created'
                         }
                         return make_response(jsonify(responseObject))
                     else:
                         responseObject = {
                             'status': 'fail',
-                            'message':'Activity Already exists'
+                            'message': 'Activity Already exists'
                         }
                         return make_response(jsonify(responseObject))
 
