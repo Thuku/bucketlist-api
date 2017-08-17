7155691015815974c4398412a91db8d983de398a
Thuku <thuku.bonnie@gmail.com>
2017-08-17 15:34:30 +0300

    [Fix] Change password hash from binary to string

--- app/models/Models.py
+++ app/models/Models.py
@@ -14,7 +14,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_name = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.Binary(255), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.datetime.now)
     updated_at = db.Column(db.DateTime, default=datetime.datetime.now,
                            onupdate=datetime.datetime.now),
