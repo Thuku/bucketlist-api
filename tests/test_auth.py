3fea20943cecd6a75d30a82546baceccfb112491
Thuku <thuku.bonnie@gmail.com>
2017-08-18 10:05:06 +0300

    [Fix] Fix tests to work with json dat

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -8,7 +8,6 @@ app = create_app("testing")
 
 class Base(unittest.TestCase):
 
-
     def setUp(self):
 
         self.client = app.test_client
@@ -28,30 +27,25 @@ class Base(unittest.TestCase):
             "username": "superman",
             "password": "password",
             "confirm_password": "password",
-            "email": "superman@test.com"}
-        ),
-        content_type='application/json'
-        )
+            "email": "superman@test.com"}), content_type='application/json')
         self.assertEqual(response.status_code, 201)
+
     def test_user_login(self):
         response_register = self.test_user_registration()
         response = self.client().post('/authentication/login', data=json.dumps({
             "username": "superman",
             "password": "password"
-        }
-        ),
-        content_type='application/json'
-        )
+        }), content_type='application/json')
         self.assertEqual(response.status_code, 200)
+
     def test_incorrect_user_registration(self):
-        response = self.client().post('/authentication/register', data=dict(
-            username="superman",
-            password="password",
-            confirm_password="passwo",
-            email="superman@test.com"
-        ), content_type='application/json'
-        )
-        self.assertEqual(response.status_code, 400)
+        response = self.client().post('/authentication/register', data=json.dumps({
+            "username": "superman",
+            "password": "password",
+            "confirm_password": "passwo",
+            "email": "superman@test.com"
+        }), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
         response = self.client().post('/authentication/register', data=json.dumps({
             "username": "superman",
             "password": "password",
@@ -65,14 +59,13 @@ class Base(unittest.TestCase):
             "confirm_password": "password",
             "email": "superman@test.com"}
         ),
-        content_type='application/json')
+            content_type='application/json')
         self.assertEqual(response.status_code, 200)
-        response = self.client().post('/authentication/register', data=dict(
-            username="superman",
-            password="password",
-            confirm_password="password"        ))
+        response = self.client().post('/authentication/register', data=json.dumps({
+            "username": "superman",
+            "password": "password",
+            "confirm_password": "password"}))
         self.assertEqual(response.status_code, 400)
-
 
 
 if __name__ == "__main__":
